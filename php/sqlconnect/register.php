4d0b2d9ff70e92f59cb9147205c4a4985c3133f9
Mateus N <mateuslimaneri@gmail.com>
2020-05-13 20:34:24 -0300

    #2 - PHP part - put it in <your mamp or xampp or wampp folder>/htdocs

---
+++ php/sqlconnect/register.php
@@ -0,0 +1,29 @@
+<?php
+$con = mysqli_connect('localhost', 'root', '', 'unitymysql');
+
+if (mysqli_connect_error()) {
+  echo ("Error -> Connection Error (error code #1)");
+  exit();
+}
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$nameCheckQuery = "SELECT username FROM players WHERE username='${username}';";
+$nameCheck = mysqli_query($con, $nameCheckQuery) 
+  or die("Error -> Name Check Query failed (error code #2)");
+
+if (mysqli_num_rows($nameCheck) > 0) {
+  echo ("Error -> Name already exists. Cannot register (error code #3)");
+  exit();
+}
+
+$salt = "\$5\$rounds=500\$" . "steamedhams" . $username . "\$";
+$hash = crypt($password, $salt);
+
+$insertUserQuery = "INSERT INTO players (username, hash, salt, email) VALUES ('${username}', '${hash}', '${salt}', '${email}'); ";
+mysqli_query($con, $insertUserQuery) 
+  or die("Error -> Insert query failed (error code #4)");
+
+echo ("teste");
