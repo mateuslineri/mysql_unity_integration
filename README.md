ae49e57424dcd0343aa63b10a9c48e3eeb8670a8
Mateus N <mateuslimaneri@gmail.com>
2020-05-14 20:26:23 -0300

    Added README.md and a structure to create the database

---
+++ README.md
@@ -0,0 +1,13 @@
+# Unity and MySQL integration
+
+This project tries to integrate Unity with MySQL database using C# and PHP for better backend security (with sanitize techniques someday in future). 
+- Unity version: 2019.3.13f1
+- PHP version 7.4.4 +
+
+## Before cloning this repository
+First! You need to have some php server solution like XAMPP or WAMP or MAMP or whatever.
+Install it and clone the repository.
+
+## After cloning this repository
+Move the **sqlconnect** folder inside **php** folder to **htdocs** or similar.
+Open the project in Unity and try it. 
--- php/sqlconnect/utils.php
+++ php/sqlconnect/utils.php
@@ -1,13 +1,54 @@
 <?php 
 
-function DBconnect()
+function DBconnect($db = 'unitymysql')
 {
-  $con = mysqli_connect('localhost', 'root', '', 'unitymysql');
+  $env = parse_ini_file(realpath('env.ini'));
+  $con = mysqli_connect($env['host'], $env['username'], $env['password'], $db);
 
   if (mysqli_connect_error()) {
-  echo ("Error -> Connection Error on register (error code #1)");
-  exit();
+    echo ("Error -> Connection Error on register (error code #1)");
+    exit();
   }
 
   return $con;
 }
+
+function createDBsctructure() {
+  $connection = DBconnect(null);
+  $createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS unitymysql;";
+
+  $createDatabase = $connection->query($createDatabaseQuery);
+
+  if ($createDatabase) {
+    echo "Database created";
+
+    $connection = DBconnect();
+    $createTableQuery = "CREATE TABLE IF NOT EXISTS players (
+      id INT(6) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+      username VARCHAR(50) NOT NULL UNIQUE,
+      hash VARCHAR(100) NOT NULL,
+      salt VARCHAR(100) NOT NULL,
+      score INT(6) DEFAULT 0,
+      level INT(6) DEFAULT 1,
+      email VARCHAR(50) NOT NULL
+    );";
+
+    $createTable = $connection->query($createTableQuery);
+
+    if ($createTable) {
+      echo "Table created";
+    }
+    else {
+      echo "Table not created. Error: " . $connection->error;
+    }
+
+    $connection->close();
+  }
+  else {
+    echo "Database not created. Error: " . $connection->error;
+  }
+
+  $connection->close();
+}
+
+createDBsctructure();
